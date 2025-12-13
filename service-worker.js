7b82efe1efaee706c22d8a30f79cf26addeae1d6
manimoeinpourofficial-hub <manimoeinpourofficial@gmail.com>
2025-12-13 04:46:21 +0330

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,44 +1,42 @@
-const CACHE_NAME = "eat-pizza-v1";
+const CACHE_NAME = "eatpizza-cache-v1";
 
 const ASSETS = [
   "./",
   "./index.html",
-  "./style.css",
   "./script.js",
   "./manifest.json",
-  "./pizzaback.jpg",
   "./PIZZA-KHOOR.png",
+  "./skin1.png",
+  "./skin2.png",
   "./pizza1.png",
   "./DRUG.png",
   "./weed.webp",
   "./shit.webp",
   "./bullet.png",
   "./speed.png",
-  "./avatar1.png",
-  "./avatar2.png",
-  "./avatar3.png",
-  "./avatar4.png",
+  "./pizza44.png",
   "./background.mp3",
   "./1.mp3",
   "./2.mp3",
   "./3.mp3",
   "./4.mp3",
   "./5.mp3",
-  "./weed.mp3",
   "./gooz1.mp3",
   "./gameover.mp3",
-  "./icon-192.png",
-  "./icon-512.png"
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
 
-self.addEventListener("install", e => {
-  e.waitUntil(
+// نصب
+self.addEventListener("install", event => {
+  event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
 });
 
-self.addEventListener("activate", e => {
-  e.waitUntil(
+// فعال‌سازی نسخهٔ جدید و پاک کردن کش‌های قدیمی
+self.addEventListener("activate", event => {
+  event.waitUntil(
     caches.keys().then(keys =>
       Promise.all(
         keys
@@ -49,8 +47,15 @@ self.addEventListener("activate", e => {
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+// پاسخ‌دهی از کش، و اگر نبود از شبکه
+self.addEventListener("fetch", event => {
+  const req = event.request;
+  if (req.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(req).then(cached => {
+      if (cached) return cached;
+      return fetch(req);
+    })
   );
 });
